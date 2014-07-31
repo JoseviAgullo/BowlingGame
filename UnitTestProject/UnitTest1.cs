f2b67460bee4f1638a4c7c7edad4cf848a1e8766
Josevi <josevi.agullo@gmail.com>
2014-07-31 11:08:00 +0200

    Tested score method
    We want the real score, not a fixed number

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -8,13 +8,28 @@ namespace UnitTestProject
     public class UnitTest1
     {
         [TestMethod]
-        public void TestGame()
+        public void testGutterGame()
+        {
+            Game game = new Game();
+            for (int i = 0; i < 20; i++)
+            {
+                game.roll(0);
+            }
+
+            Assert.AreEqual(0, game.score());
+        }
+
+        [TestMethod]
+        public void testAllFour()
         {
             Game game = new Game();
 
-            game.roll(0);
+            for (int i = 0; i < 20; i++)
+            {
+                game.roll(4);
+            }
 
-            Assert.AreEqual(0, game.score());
+            Assert.AreEqual(80, game.score());
         }
     }
 }
